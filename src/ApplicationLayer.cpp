d201e7c45d8c981ddc2692cb932bd56fff018d9f
estevamgalvao <estevamgalvao1@gmail.com>
2021-03-30 10:32:35 +0100

    string to bitset - bitset to char

--- src/ApplicationLayer.cpp
+++ src/ApplicationLayer.cpp
@@ -4,4 +4,9 @@ ApplicationLayer::ApplicationLayer(){};
 
 void ApplicationLayer::start(int flag, std::string m) {
 
+    for (size_t i = 0; i < m.size(); i++)
+    {
+        std::cout << m[i] << " ";
+        bitstream_.push_back((std::bitset<BYTE_LENGTH>) m[i]);
+    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -5,30 +5,29 @@
 int main(int argc, char const *argv[])
 {
     std::string input;
-    std::cin >> input;
-
+    std::getline (std::cin,input);
     ApplicationLayer appLayer;
     
     std::vector<std::bitset<BYTE_LENGTH>> teste;
     std::vector<std::bitset<BYTE_LENGTH>>::iterator it;
-    std::bitset<BYTE_LENGTH> b1('A');
-    
+
     for (size_t i = 0; i < input.size(); i++)
     {
         std::cout << input[i] << " ";
         teste.push_back((std::bitset<BYTE_LENGTH>) input[i]);
     }
     
-    teste.push_back(b1);
-
-
-    
     for (size_t i = 0; i < teste.size(); i++)
     {
         std::cout << "\n" << teste[i] << "\n";
     }
+    std::cout << "\nSTRING\n";
     
-
+    for (size_t i = 0; i < teste.size(); i++)
+    {
+        std::cout << (char) teste[i].to_ulong() << "\n"; // A linguagem sabe transformar u long int para char, por isso fazemos assim
+    }
+    
 
     return 0;
 }
